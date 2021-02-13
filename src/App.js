9f0bdde83c7d145f8adf12c08ba66ddfe28325e3
janellebaguley <nellie.baguley@gmail.com>
2021-02-13 14:51:18 -0700

    contact update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom'
+import routes from './routes'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      {routes}
     </div>
   );
 }
