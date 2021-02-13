24c8bd6f95c53507528e6f747f8a190646bd9a6d
janellebaguley <nellie.baguley@gmail.com>
2021-02-13 14:27:25 -0700

    update

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express'),
+      ctrl = require('./controller')
+const {SERVER_PORT} = process.env
+
+const app = express()
+app.use(express.json())
+
+//nodemailer endpoint
+
+app.post('/api/mail', ctrl.email)
+
+app.listen(SERVER_PORT, () => console.log(`Running on port: ${SERVER_PORT}`))
